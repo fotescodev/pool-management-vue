3747b1a0fe44c05098ec3ed8b772ead61246ed80
Dmitrii Fotesco <dmitrii@purestake.com>
2021-02-17 02:14:42 -0800

    Added a configuration file registry.moonbase.json

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,16 @@
 import merge from 'lodash/merge';
 import registry from '@balancer-labs/assets/generated/pm/registry.homestead.json';
 import registryKovan from '@balancer-labs/assets/generated/pm/registry.kovan.json';
+import registryMoonbase from '@balancer-labs/assets/generated/pm/registry.moonbase.json';
 import homestead from '@/config/homestead.json';
 import kovan from '@/config/kovan.json';
+import moonbase from '@/config/moonbase.json';
 
-const configs = { homestead, kovan };
+const configs = { homestead, kovan, moonbase };
 configs.homestead = merge(registry, configs.homestead);
 configs.kovan = merge(registryKovan, configs.kovan);
-const network = process.env.VUE_APP_NETWORK || 'homestead';
+configs.moonbase = merge(registryMoonbase, configs.moonbase);
+const network = process.env.VUE_APP_NETWORK || 'moonbase';
 const config = configs[network];
 config.env = process.env.VUE_APP_ENV || 'staging';
 
